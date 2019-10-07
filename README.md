6a7b4616bc51327ab4ee50ac946a9e6c288c4b2c
dxd <dxd_sjtu@outlook.com>
2019-10-07 17:15:22 +0200

    Feat create certificate (#2)
    * feat: create certificate

---
+++ README.md
@@ -0,0 +1,20 @@
+# https-pool
+
+A cached pool of https server for domains.
+
+---
+
+[![NPM version](https://img.shields.io/npm/v/https-pool.svg?style=flat)](https://npmjs.org/package/https-pool)
+[![Build Status](https://img.shields.io/travis/ahungrynoob/https-pool.svg?style=flat)](https://travis-ci.org/ahungrynoob/https-pool)
+[![codecov.io](https://img.shields.io/codecov/c/github/ahungrynoob/https-pool.svg?style=flat)](http://codecov.io/github/ahungrynoob/https-pool?branch=master)
+[![NPM downloads](http://img.shields.io/npm/dm/https-pool.svg?style=flat)](https://npmjs.org/package/https-pool)
+
+## Install
+
+```bash
+$ npm i https-pool --save
+```
+
+## LICENSE
+
+MIT
---
+++ test/lib/utils.test.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const mm = require('mm');
+const path = require('path');
+const forge = require('node-forge');
+const fs = require('fs');
+const assert = require('assert');
+const { createCACert, createCertificate } = require('../../lib/utils');
+
+const pki = forge.pki;
+
+const fixtures = path.join(__dirname, '../fixtures');
+
+// const certificate = createCertificate('www.taobao.com', ca.key, ca.cert);
+
+describe('lib/utils', () => {
+  afterEach(mm.restore);
+  it('should generate correct root ca pem', () => {
+    mm(forge.pki.rsa, 'generateKeyPair', function() {
+      const privateKeyPem = fs.readFileSync(path.join(fixtures, 'root.key'));
+      const privateKey = pki.privateKeyFromPem(privateKeyPem);
+      const publicKey = pki.setRsaPublicKey(privateKey.n, privateKey.e);
+      return { privateKey, publicKey };
+    });
+
+    const RealDate = Date;
+
+    mm(global, 'Date', class extends RealDate {
+      constructor() {
+        return new RealDate(1559742360000);
+      }
+
+      getFullYear() {
+        return 2019;
+      }
+    });
+
+    const ca = createCACert({
+      commonName: 'whistle.1559742360747607',
+      countryName: 'CN',
+      ST: 'ZJ',
+      localityName: 'HZ',
+      organizationName: '1559742360747607.wproxy.org',
+      OU: 'wproxy.org',
+    });
+
+    const caPem = pki.certificateToPem(ca.cert);
+    const rootCrt = fs.readFileSync(path.join(fixtures, 'root.crt'), 'utf8');
+    assert.equal(caPem.replace(/[\r\n]/g, ''), rootCrt.replace(/[\r\n]/g, ''));
+  });
+
+  it('should generate correct certificate based on hostname', () => {
+    const privateKeyPem = fs.readFileSync(path.join(fixtures, 'root.key'));
+    const privateKey = pki.privateKeyFromPem(privateKeyPem);
+    const certPem = fs.readFileSync(path.join(fixtures, 'root.crt'));
+    const cert = pki.certificateFromPem(certPem);
+
+    const certificateDomain = createCertificate('www.taobao.com', privateKey, cert);
+    assert.equal(
+      pki.certificateFromPem(certificateDomain.cert).subject.attributes[0].value,
+      'www.taobao.com'
+    );
+
+    const certificateIP = createCertificate('192.168.1.1', privateKey, cert);
+    assert.equal(
+      pki.certificateFromPem(certificateIP.cert).subject.attributes[0].value,
+      '192.168.1.1'
+    );
+  });
+});
